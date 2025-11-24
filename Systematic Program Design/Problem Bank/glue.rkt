63e85374b50f01737d4d60fdf9f86b14bafae620
Evelyn Park <hello@evelynpark.com>
2025-11-23 23:06:22 -0500

    more sensible files

---
+++ Systematic Program Design/Problem Bank/glue.rkt
@@ -0,0 +1,3 @@
+#lang htdp/bsl
+
+(string-append "Super" " " "Glue")
---
+++ Systematic Program Design/Problem Bank/tile.rkt
@@ -0,0 +1,5 @@
+#lang htdp/bsl
+
+(require 2htdp/image)
+
+(above (beside (rectangle 20 20 "solid" "blue") (rectangle 20 20 "solid" "yellow")) (beside (rectangle 20 20 "solid" "yellow") (rectangle 20 20 "solid" "blue")))
